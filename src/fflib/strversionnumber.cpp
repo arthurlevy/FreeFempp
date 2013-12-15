21ac0a85a971a4e0f0087df0a2ce39e43aa5e2a7
Dimitrios Eftaxiopoulos <eftaxi12@otenet.gr>
2013-12-15 23:55:37 +0200

    Correct architecture names for hurd and kfreebsd

--- src/fflib/strversionnumber.cpp
+++ src/fflib/strversionnumber.cpp
@@ -18,6 +18,6 @@ string StrVersionNumber(){
 //  buffer.precision(8);
 //  buffer<<VersionNumber();
   static char buffer[100];
-  sprintf(buffer," %9f (date Κυρ 15 Δεκ 2013 11:19:01 μμ EET)",VersionNumber());
-  return buffer; //.str()+" (date Κυρ 15 Δεκ 2013 11:19:01 μμ EET)" ;
+  sprintf(buffer," %9f (date Κυρ 15 Δεκ 2013 11:34:40 μμ EET)",VersionNumber());
+  return buffer; //.str()+" (date Κυρ 15 Δεκ 2013 11:34:40 μμ EET)" ;
 }
