44dcf962a51c1dd7a4f23ba8220b0d895b46d638
Dimitrios Eftaxiopoulos <eftaxi12@otenet.gr>
2013-12-17 23:55:45 +0200

    Fix duplication of executables and FTBS on kfreeBSD and Hurd

--- src/fflib/strversionnumber.cpp
+++ src/fflib/strversionnumber.cpp
@@ -18,6 +18,6 @@ string StrVersionNumber(){
 //  buffer.precision(8);
 //  buffer<<VersionNumber();
   static char buffer[100];
-  sprintf(buffer," %9f (date Κυρ 15 Δεκ 2013 11:57:21 μμ EET)",VersionNumber());
-  return buffer; //.str()+" (date Κυρ 15 Δεκ 2013 11:57:21 μμ EET)" ;
+  sprintf(buffer," %9f (date Jeu 28 nov 2013 11:11:34 CET)",VersionNumber());
+  return buffer; //.str()+" (date Jeu 28 nov 2013 11:11:34 CET)" ;
 }
