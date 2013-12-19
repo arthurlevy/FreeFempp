c3d44032f196f65b116a6c26c977e4f4b98f005f
Dimitrios Eftaxiopoulos <eftaxi12@otenet.gr>
2013-12-19 22:23:27 +0200

    Try pbuilder with parallel build

--- src/fflib/strversionnumber.cpp
+++ src/fflib/strversionnumber.cpp
@@ -18,6 +18,6 @@ string StrVersionNumber(){
 //  buffer.precision(8);
 //  buffer<<VersionNumber();
   static char buffer[100];
-  sprintf(buffer," %9f (date Πεμ 19 Δεκ 2013 05:32:08 μμ EET)",VersionNumber());
-  return buffer; //.str()+" (date Πεμ 19 Δεκ 2013 05:32:08 μμ EET)" ;
+  sprintf(buffer," %9f (date Πεμ 19 Δεκ 2013 08:27:13 μμ EET)",VersionNumber());
+  return buffer; //.str()+" (date Πεμ 19 Δεκ 2013 08:27:13 μμ EET)" ;
 }
